62e5341bdfbf1b363c2e3e504ea30c44e0a1a4cc
Sahanah <sahanah.ganesh@gmail.com>
2019-11-14 16:59:48 -0500

    changes nav colors

--- src/shared/nav/nav.component.tsx
+++ src/shared/nav/nav.component.tsx
@@ -8,31 +8,31 @@ interface INavProps {
 }
 
 export const Nav: React.SFC<INavProps> = ({ home }) => (
-  <Box width="100%" bg={theme.colors.white} p={3}>
-    <Flex color="black" bg="white" alignItems="center">
+  <Box width="100%" bg={theme.colors.purple} p={3}>
+    <Flex color="black" bg={theme.colors.purple} alignItems="center">
       <Router>
-        <Link style={{ textDecoration: "none" }} to="/">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/">
           <Text pl={5} fontSize={theme.fontSizes[5]} fontWeight="bold">
             Dreamsicle
           </Text>
         </Link>
         <Box mx="auto" />
-        <Link style={{ textDecoration: "none" }} to="/about">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/about">
           <Text pr={5} fontSize={theme.fontSizes[3]}>
             About
           </Text>
         </Link>
-        <Link style={{ textDecoration: "none" }} to="/demo">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/demo">
           <Text pr={5} fontSize={theme.fontSizes[3]}>
             Demo
           </Text>
         </Link>
-        <Link style={{ textDecoration: "none" }} to="/team">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/team">
           <Text pr={5} fontSize={theme.fontSizes[3]}>
             Team
           </Text>
         </Link>
-        <Link style={{ textDecoration: "none" }} to="/connect">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/connect">
           <Text pr={5} fontSize={theme.fontSizes[3]}>
             Connect
           </Text>
--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -2,6 +2,7 @@ export const theme = {
   breakpoints: ["834px", "1112px", "1280px", "1440px"],
   colors: {
     white: "#FFFFFF",
+    purple: "#6B4984",
   },
   fontSizes: [14, 16, 18, 24, 36, 48],
 }
